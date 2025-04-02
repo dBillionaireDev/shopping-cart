db2e10f2a3b4e7029b7903763488d937eba721a9
Nezer-Ekunke <nezerekunke@gmail.com>
2025-04-02 01:47:22 +0100

    still working

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import data from '/home/nexy/React-Projects/Product-Cart-FM/src/data.json';
-import Order from './Order'; 
 import cart from '../assets/images/icon-add-to-cart.svg';
+import decrementIcon from '../assets/images/icon-decrement-quantity.svg';
+import incrementIcon from '../assets/images/icon-increment-quantity.svg';
 
 const Cart = () => {
     const [count, setCount] = useState('')
+    const [quantity, setQuantity] = useState(1)
     function handleSubmit(){
 
     }
@@ -16,7 +18,8 @@ const Cart = () => {
             <section className="cart-item">
                 <li key={Math.random()}><img className="cart-img" src={item.image.desktop} alt='item.name'/></li>
                 <button className="submit-btn" onClick={handleSubmit} type="submit"><img src={cart} alt="cart-icon" /> Add to Cart</button>
-                <button className="state" onClick={()=> {setCount(count - 1)}}>-</button>{count}<button className="state" onClick={() => {setCount(count + 1)}}>+</button>
+                <button className="state"> <button className='quantity--cart--dec' onClick={()=> {setCount(count - 1)}}><img src={decrementIcon} alt='reduce item' /></button> {quantity} <button className='quantity--cart--add' onClick={()=> {setCount(count + 1)}}><img src={incrementIcon} alt='increase item' /></button> </button>
+                {/* <button className="state" onClick={()=> {setCount(count - 1)}}>-</button>{count}<button className="state" onClick={() => {setCount(count + 1)}}>+</button> */}
               <span className='des'>
                 <li key={Math.random()} style={{ color: 'hsl(12, 20%, 44%)'}}>{item.category}</li><li key={Math.random()}><strong>{item.name}</strong></li>
                 <li key={Math.random()} style={{ color: 'hsl(14, 65%, 38%)', }}><strong>${item.price}</strong></li>
@@ -24,9 +27,7 @@ const Cart = () => {
             </section>
         ))}
         </ul>
-          {/* <Order className='desktop' /> */}
   
-        <button className="state" onClick={()=> {setCount(count - 1)}}>-</button>{count}
     </div>
   )
 }
