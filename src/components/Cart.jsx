53ecd3faf97df6ace5c8ba0775d948c8f002dcb7
Nezer-Ekunke <nezerekunke@gmail.com>
2025-04-03 15:45:08 +0100

    commit new

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,28 +1,59 @@
 import React, { useState } from 'react'
 import data from '/home/nexy/React-Projects/Product-Cart-FM/src/data.json';
-import cart from '../assets/images/icon-add-to-cart.svg';
+import cartImg from '../assets/images/icon-add-to-cart.svg';
 import decrementIcon from '../assets/images/icon-decrement-quantity.svg';
 import incrementIcon from '../assets/images/icon-increment-quantity.svg';
 
 const Cart = () => {
-    const [count, setCount] = useState('')
-    const [quantity, setQuantity] = useState(1)
-    function handleSubmit(){
+    const [cart, setCart] = useState({});
+    const [styles, setStyles] = useState({});
 
-    }
-    
+    const updateQuantity = (id, amount) => {
+      setCart((prev) => {
+        const newQuantity = (prev[id] || 0) + amount;
+        const newCart = { ...prev };
+  
+        if (newQuantity <= 0) {
+          delete newCart[id]; 
+        } else {
+          newCart[id] = newQuantity;
+        }
+  
+        setStyles((prevStyles) => ({
+          ...prevStyles,
+          [id]: {
+            border: newQuantity > 0 ? "3px solid hsl(14, 86%, 42%)" : "",
+            boxShadow: newQuantity > 0 ? "0px 4px 10px rgba(0, 0, 255, 0.2)" : "none",
+          },
+        }));
+  
+        return newCart;
+      });
+    };
+
   return (
     <div id="cart">
         <ul className="product-cart">
         {data.map((item) => (
-            <section className="cart-item">
-                <li key={Math.random()}><img className="cart-img" src={item.image.desktop} alt='item.name'/></li>
-                <button className="submit-btn" onClick={handleSubmit} type="submit"><img src={cart} alt="cart-icon" /> Add to Cart</button>
-                <button className="state"> <button className='quantity--cart--dec' onClick={()=> {setCount(count - 1)}}><img src={decrementIcon} alt='reduce item' /></button> {quantity} <button className='quantity--cart--add' onClick={()=> {setCount(count + 1)}}><img src={incrementIcon} alt='increase item' /></button> </button>
-                {/* <button className="state" onClick={()=> {setCount(count - 1)}}>-</button>{count}<button className="state" onClick={() => {setCount(count + 1)}}>+</button> */}
-              <span className='des'>
-                <li key={Math.random()} style={{ color: 'hsl(12, 20%, 44%)'}}>{item.category}</li><li key={Math.random()}><strong>{item.name}</strong></li>
-                <li key={Math.random()} style={{ color: 'hsl(14, 65%, 38%)', }}><strong>${item.price}</strong></li>
+      
+            <section className="cart-item" key={item.id}>
+                <li><img className="cart-img" src={item.image.desktop} alt='item.name'  style={{...styles[item.id],}} /></li>
+    
+                {cart[item.id] ? (
+                  <span className="state">
+                    <button className='quantity--cart--dec' onClick={() => updateQuantity(item.id, -1)}><img src={decrementIcon} alt='increase item' /></button>
+                    <span> {cart[item.id]} </span>
+                    <button className='quantity--cart--add' onClick={() => updateQuantity(item.id, 1)}><img src={incrementIcon} alt='increase item' /></button>
+                  </span>
+                ) : (
+                  <button className="submit-btn" onClick={() => updateQuantity(item.id, 1)}><img src={cartImg} alt="cart-icon" /> Add to Cart</button>
+                )}
+              <span  className='des'>
+                <li style={{ color: 'hsl(12, 20%, 44%)'}}>{item.category}</li>
+                <li><strong>{item.name}</strong></li>
+
+                <li style={{ color: 'hsl(14, 65%, 38%)', }}><strong>${item.price}</strong></li>
+
               </span>
             </section>
         ))}
